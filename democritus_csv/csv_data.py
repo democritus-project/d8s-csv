ec5c18dd96ecc9d420be4c2970bf3e017b40a8b4
Floyd Hightower <floyd.hightower27@gmail.com>
2021-01-18 18:38:13 -0500

    Removing for non-existent functionality
    See https://github.com/democritus-project/democritus-csv/issues/2

--- democritus_csv/csv_data.py
+++ democritus_csv/csv_data.py
@@ -3,10 +3,10 @@ from io import StringIO
 
 
 def csv_read_as_list(csv_data: str, **kwargs):
-    """Read the csv_data (which can be a URL, file path, or raw CSV) as a list."""
+    """Read the csv_data as a list."""
     yield from csv.reader(StringIO(csv_data), **kwargs)
 
 
 def csv_read_as_dict(csv_data: str, **kwargs):
-    """Read the csv_data (which can be a URL, file path, or raw CSV) as a dict."""
+    """Read the csv_data as a dict."""
     yield from csv.DictReader(StringIO(csv_data), **kwargs)
--- tests/test_csv_data.py
+++ tests/test_csv_data.py
@@ -28,15 +28,17 @@ def test_csv_read_as_dict_docs_1():
     assert tuple(csv_read_as_dict(TEXT_COMMA_DELIMITED)) == ({'foo': '1', 'bar': '2'},)
 
 
-@pytest.mark.network
-def test_csv_read_as_dict_url_argument():
-    """Make sure the csv_read_as_dict function will properly request the content from a url when given a url."""
-    result = tuple(csv_read_as_dict(
-        'https://www.iana.org/assignments/iana-as-numbers-special-registry/special-purpose-as-numbers.csv'
-    ))
-    assert isinstance(result, tuple)
-    assert len(result) == 10
-    assert 'AS Number' in result[0]
-    assert 'Reason for Reservation' in result[0]
-    assert 'Reference' in result[0]
-    assert result[0] == {'AS Number': '0', 'Reason for Reservation': 'Reserved by [RFC7607]', 'Reference': '[RFC7607]'}
+# @pytest.mark.network
+# def test_csv_read_as_dict_url_argument():
+#     """Make sure the csv_read_as_dict function will properly request the content from a url when given a url."""
+#     result = tuple(
+#         csv_read_as_dict(
+#             'https://www.iana.org/assignments/iana-as-numbers-special-registry/special-purpose-as-numbers.csv'
+#         )
+#     )
+#     assert isinstance(result, tuple)
+#     assert len(result) == 10
+#     assert 'AS Number' in result[0]
+#     assert 'Reason for Reservation' in result[0]
+#     assert 'Reference' in result[0]
+#     assert result[0] == {'AS Number': '0', 'Reason for Reservation': 'Reserved by [RFC7607]', 'Reference': '[RFC7607]'}
