0b5d0be2e5a3c6e6ad41875ef4150fe4d2898483
Floyd Hightower <floyd.hightower27@gmail.com>
2021-01-18 18:34:51 -0500

    Cleaning code

--- democritus_csv/csv_data.py
+++ democritus_csv/csv_data.py
@@ -9,7 +9,4 @@ def csv_read_as_list(csv_data: str, **kwargs):
 
 def csv_read_as_dict(csv_data: str, **kwargs):
     """Read the csv_data (which can be a URL, file path, or raw CSV) as a dict."""
-    # TODO: should I do something to detect failures and try with a different delimiter?
-
-    reader = csv.DictReader(StringIO(csv_data), **kwargs)
-    yield from reader
+    yield from csv.DictReader(StringIO(csv_data), **kwargs)
